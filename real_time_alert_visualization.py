d5265987502c60402a49ab09d1ab6951e22ea855
JMogiel <109518507+JMogiel@users.noreply.github.com>
2024-06-06 13:42:17 +0200

    Update real_time_alert_visualization.py

--- real_time_alert_visualization.py
+++ real_time_alert_visualization.py
@@ -2,47 +2,67 @@ from kafka import KafkaConsumer
 import json
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import threading
+import queue
 
 class AnomalyAlert:
-    def __init__(self, id, value, timestamp):
-        self.id = id
-        self.value = value
+    def __init__(self, alert_id, card_id, fraud_type, fraud_details, timestamp):
+        self.alert_id = alert_id
+        self.card_id = card_id
+        self.fraud_type = fraud_type
+        self.fraud_details = fraud_details
         self.timestamp = timestamp
+
+    def __repr__(self):
+        return f"AnomalyAlert(alert_id={self.alert_id}, card_id={self.card_id}, fraud_type={self.fraud_type}, timestamp={self.timestamp})"
 
 def deserialize_alert(message):
     data = json.loads(message)
-    return AnomalyAlert(data['id'], data['value'], data['timestamp'])
+    return AnomalyAlert(
+        data['alertId'],
+        data['cardId'],
+        data['fraudType'],
+        data['fraudDetails'],
+        data['timestamp']
+    )
 
-def update(frame, consumer, alerts, ax):
-    for message in consumer:
-        alert_data = message.value
-        alert = deserialize_alert(alert_data)
+def update(frame, alert_queue, alerts, ax):
+    while not alert_queue.empty():
+        alert = alert_queue.get()
         alerts.append(alert)
-        print(f"Received alert: {alert_data}")
-        break  # Process one message per frame update
+        print(f"Received alert: {alert}")
 
     ax.clear()
-    alert_values = [alert.value for alert in alerts]
+    alert_values = [alert.card_id for alert in alerts]
     ax.hist(alert_values, bins=20, edgecolor='black')
-    ax.set_xlabel('Alert Value')
+    ax.set_xlabel('Card ID')
     ax.set_ylabel('Frequency')
-    ax.set_title('Distribution of Alert Values')
+    ax.set_title('Distribution of Card IDs for Alerts')
 
-def consume_alerts():
+def kafka_consumer_thread(alert_queue):
     consumer = KafkaConsumer(
         'test-alert',
         bootstrap_servers=['localhost:9092'],
         auto_offset_reset='earliest',
         enable_auto_commit=True,
         group_id='alert-group',
-        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+        value_deserializer=lambda x: x.decode('utf-8')
     )
 
+    for message in consumer:
+        alert = deserialize_alert(message.value)
+        alert_queue.put(alert)
+
+def consume_alerts():
+    alert_queue = queue.Queue()
     alerts = []
 
-    fig, ax = plt.subplots()
-    ani = FuncAnimation(fig, update, fargs=(consumer, alerts, ax), interval=1000)
+    consumer_thread = threading.Thread(target=kafka_consumer_thread, args=(alert_queue,))
+    consumer_thread.daemon = True
+    consumer_thread.start()
 
+    fig, ax = plt.subplots()
+    ani = FuncAnimation(fig, update, fargs=(alert_queue, alerts, ax), interval=1000)
     plt.show()
 
 if __name__ == "__main__":
